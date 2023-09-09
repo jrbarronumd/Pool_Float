dadba8a521e008f38982418ff741797873d9aceb
jrbarronumd <39811147+jrbarronumd@users.noreply.github.com>
2023-09-09 10:13:22 -0400

    Basic code draft

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 // pH temp compensation: https://www.hach.com/asset-get.download.jsa?id=17525673904
 // deep sleep????  RTC pull-up resistor for charge pump to shut down in deep sleep!!!
 // Need to add a software switch to keep the ESP out of sleep mode in case of desired OTA software update.
+// TODO:
+	// For PCB:
+		// WHY does the TP4056 not work???
+		// Add 3.3V, 5V, and GND header pins to the J-tag pins (or somewhere else on the top face)
+		// ESP antenna should not be surrounded by interference...
+		// Change pinout of orp/pH connector so each 3-pin connector can be directly plugged in
 
 #include <Arduino.h>
 #include <OneWire.h>
@@ -13,7 +19,7 @@
 #include "EspMQTTClient.h"
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
-#include <SPI.h> // Not having this caused build to fail - dependency of ADS1X15 library? 
+#include <SPI.h> // Not having this caused build to fail - dependency of ADS1X15 library?
 
 #define poolTempPin 23
 #define chgPumpPin 17 // Shutdown pin on the 5V charge pump
@@ -31,9 +37,9 @@ EspMQTTClient client(
 );
 
 // Constants based on calibration:
-float phSlope = -1.0 * 3.0 / 4240.0; // (At calTemp)
-float phIntercept = 4.0 - phSlope * 16544.0;
-float calTemp = 23.6; // pH calibration temp in deg C (temp of pH buffer used to generate phSlope)
+float phSlope = -0.00107411; // in pH/bit - WILL NEED TO CHANGE IF ADS GAIN CHANGES!!
+float phIntercept = 15.76451632; // WILL NEED TO CHANGE IF ADS GAIN CHANGES!!
+float calTemp = 22.56; // pH calibration temp in deg C (temp of pH buffer used to generate phSlope)
 
 // Variables
 float orp = 0.0; // Oxidation reduction potential - used to estimate chlorine concentration
@@ -55,33 +61,33 @@ void onConnectionEstablished() {
 }
 
 void getPH() {
-	digitalWrite(phPin, HIGH); // This will power the pH probe amp board while reading only.
-	delay(2000);
+	// digitalWrite(phPin, HIGH); // This will power the pH probe amp board.
+	// delay(2000);
 // ********************Need While loop here to make sure values aren't changing.***************************
-	phBits = ads.readADC_SingleEnded(0);
+	phBits = ads.readADC_SingleEnded(1);
 	float phSlopeComp = phSlope * (tempC + 273.15) / (calTemp + 273.15); // Temp compensation (Source listed above)
 	pH = phSlopeComp * phBits + phIntercept;
-	digitalWrite(phPin, LOW);
-	delay(500);
+	// digitalWrite(phPin, LOW);
+	// delay(500);
 }
 
 void getORP() {
-	digitalWrite(orpPin, HIGH); // This will power the ORP probe amp board while reading only.
-	delay(2000);
+	// digitalWrite(orpPin, HIGH); // This will power the ORP probe amp board
+	// delay(2000);
 // ********************Need While loop here to make sure values aren't changing.***************************
-	orp = ads.readADC_SingleEnded(2); // USE COMPARISON TO GROUND INSTEAD?
-	orp = orp * .125; // converting bits to mV
-	digitalWrite(orpPin, LOW);
-	delay(500);
+	orp = ads.readADC_SingleEnded(0); // USE COMPARISON TO GROUND INSTEAD?
+	orp = orp * 0.1875; // converting bits to mV
+	// digitalWrite(orpPin, LOW);
+	// delay(500);
 }
 
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(115200);
 	pinMode(phPin, OUTPUT);
 	pinMode(orpPin, OUTPUT);
 	pinMode(chgPumpPin, OUTPUT);
-	digitalWrite(phPin, LOW);
-	digitalWrite(orpPin, LOW);
+	digitalWrite(phPin, HIGH);
+	digitalWrite(orpPin, HIGH);
 	digitalWrite(chgPumpPin, LOW);
 	Serial.println("ADC Range: +/- 6.144V (1 bit = 0.1875mV)");
 	ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 0.1875mV (default)
@@ -93,7 +99,7 @@ void setup() {
 	client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overwritten with enableHTTPWebUpdater("user", "password").
 	client.enableLastWillMessage("poolFloat/status", "offline", true);  // You can activate the retain flag by setting the third parameter to true
 	client.setKeepAlive(600); // (in seconds) Default is 15 seconds.
-	// client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+	client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
 }
 
 
@@ -105,13 +111,11 @@ void loop() {
 		sensors.requestTemperatures();
 		tempF = sensors.getTempFByIndex(0);
 		tempC = sensors.getTempCByIndex(0);
-		//getPH();
-		//getORP();
-		Serial.print(tempF); Serial.print(" F -- ");
-		Serial.print(tempC); Serial.print(" C -- ");		
-		Serial.print(phBits); Serial.print(" bits --");		
-		Serial.print(" pH = "); Serial.print(pH); Serial.print(" -- ");
-		Serial.print(orp); Serial.println(" mV");
+		getPH();
+		getORP();
+		String msg = "";
+		msg = String(msg + tempF + " F -- " + tempC + " C -- " + phBits + " bits -- pH = " + pH + " --  ORP = " + orp + " mV");
+		Serial.println(msg);
+		client.publish("poolFloat/data", msg, true);
 	}
-	// Code to Run every loop
 }
